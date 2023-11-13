f57f32090fedb282029a38e034f7224bb204d582
rusko1983 <rusmir012@gmail.com>
2023-11-13 11:19:25 +0100

    rusmir3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./styles.css";
+function Article() {
+  return <article className="article" >
 
+      <h2 className="article__title" >    NICE SUPER</h2>
+      <label htmlFor="name">Type something</label>
+      <input type="text" name="name" id="name" />
+       <a className="article__link" href="Wikipedia article">CLICK ME</a>
+
+  </article>
+
+}
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article/>
 }
